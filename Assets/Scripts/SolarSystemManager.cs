09e2dd7d545b7e2562b9a24c0f4bd72eb23eccf7
Elias <eliasbrucart@hardgames.com.ar>
2021-04-04 19:56:48 -0300

    Now the planets are randomized

---
+++ Assets/Scripts/SolarSystemManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class SolarSystemManager : MonoBehaviour
+{
+    public int amountPlanets = 0;
+    public GameObject[] planets;
+
+    void Start()
+    {
+        List<int> listNumbers = new List<int>();
+        int random = 0;
+        for(int i = 0; i < amountPlanets; i++)
+        {
+            do
+            {
+                random = UnityEngine.Random.Range(0, planets.Length);
+            } while (listNumbers.Contains(random));
+            listNumbers.Add(random);
+            Instantiate(planets[listNumbers[i]]);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
